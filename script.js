1a1bba685e01163ca8f61d50e6bb754ceaa00582
y <johndoe@example.com>
2022-06-02 15:29:38 +0100

    Added functionality to N

--- script.js
+++ script.js
@@ -6722,6 +6722,43 @@ let bKeyboardButton = document.querySelectorAll("button.keyboard-letter")[23];
 bKeyboardButton.addEventListener("click", guessB);
 
 
+function guessN () {
+    if (g1L1.innerHTML == "") {g1L1.innerHTML = "N";}
+    else if (g1L2.innerHTML == "") {g1L2.innerHTML = "N";}
+    else if (g1L3.innerHTML == "") {g1L3.innerHTML = "N";}
+    else if (g1L4.innerHTML == "") {g1L4.innerHTML = "N";}
+    else if (g1L5.innerHTML == "") {g1L5.innerHTML = "N";}
+    else if (g2L1.innerHTML == "") {g2L1.innerHTML = "N";}
+    else if (g2L2.innerHTML == "") {g2L2.innerHTML = "N";}
+    else if (g2L3.innerHTML == "") {g2L3.innerHTML = "N";}
+    else if (g2L4.innerHTML == "") {g2L4.innerHTML = "N";}
+    else if (g2L5.innerHTML == "") {g2L5.innerHTML = "N";}
+    else if (g3L1.innerHTML == "") {g3L1.innerHTML = "N";}
+    else if (g3L2.innerHTML == "") {g3L2.innerHTML = "N";}
+    else if (g3L3.innerHTML == "") {g3L3.innerHTML = "N";}
+    else if (g3L4.innerHTML == "") {g3L4.innerHTML = "N";}
+    else if (g3L5.innerHTML == "") {g3L5.innerHTML = "N";}
+    else if (g4L1.innerHTML == "") {g4L1.innerHTML = "N";}
+    else if (g4L2.innerHTML == "") {g4L2.innerHTML = "N";}
+    else if (g4L3.innerHTML == "") {g4L3.innerHTML = "N";}
+    else if (g4L4.innerHTML == "") {g4L4.innerHTML = "N";}
+    else if (g4L5.innerHTML == "") {g4L5.innerHTML = "N";}
+    else if (g5L1.innerHTML == "") {g5L1.innerHTML = "N";}
+    else if (g5L2.innerHTML == "") {g5L2.innerHTML = "N";}
+    else if (g5L3.innerHTML == "") {g5L3.innerHTML = "N";}
+    else if (g5L4.innerHTML == "") {g5L4.innerHTML = "N";}
+    else if (g5L5.innerHTML == "") {g5L5.innerHTML = "N";}
+    else if (g6L1.innerHTML == "") {g6L1.innerHTML = "N";}
+    else if (g6L2.innerHTML == "") {g6L2.innerHTML = "N";}
+    else if (g6L3.innerHTML == "") {g6L3.innerHTML = "N";}
+    else if (g6L4.innerHTML == "") {g6L4.innerHTML = "N";}
+    else {g6L5.innerHTML = "N";}
+}
+
+let nKeyboardButton = document.querySelectorAll("button.keyboard-letter")[24];
+nKeyboardButton.addEventListener("click", guessN);
+
+
 /*
 let eraseButton = document.getElementsByClassName("keyboard-button")[1];
 function backspace (currentGuess) {
