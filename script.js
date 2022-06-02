d65f69069f410622a236b36f47b061f2b5f4836d
y <johndoe@example.com>
2022-06-02 15:03:11 +0100

    Added functionality to H

--- script.js
+++ script.js
@@ -6387,6 +6387,42 @@ let gKeyboardButton = document.querySelectorAll("button.keyboard-letter")[14];
 gKeyboardButton.addEventListener("click", guessG);
 
 
+function guessH () {
+    if (g1L1.innerHTML == "") {g1L1.innerHTML = "H";}
+    else if (g1L2.innerHTML == "") {g1L2.innerHTML = "H";}
+    else if (g1L3.innerHTML == "") {g1L3.innerHTML = "H";}
+    else if (g1L4.innerHTML == "") {g1L4.innerHTML = "H";}
+    else if (g1L5.innerHTML == "") {g1L5.innerHTML = "H";}
+    else if (g2L1.innerHTML == "") {g2L1.innerHTML = "H";}
+    else if (g2L2.innerHTML == "") {g2L2.innerHTML = "H";}
+    else if (g2L3.innerHTML == "") {g2L3.innerHTML = "H";}
+    else if (g2L4.innerHTML == "") {g2L4.innerHTML = "H";}
+    else if (g2L5.innerHTML == "") {g2L5.innerHTML = "H";}
+    else if (g3L1.innerHTML == "") {g3L1.innerHTML = "H";}
+    else if (g3L2.innerHTML == "") {g3L2.innerHTML = "H";}
+    else if (g3L3.innerHTML == "") {g3L3.innerHTML = "H";}
+    else if (g3L4.innerHTML == "") {g3L4.innerHTML = "H";}
+    else if (g3L5.innerHTML == "") {g3L5.innerHTML = "H";}
+    else if (g4L1.innerHTML == "") {g4L1.innerHTML = "H";}
+    else if (g4L2.innerHTML == "") {g4L2.innerHTML = "H";}
+    else if (g4L3.innerHTML == "") {g4L3.innerHTML = "H";}
+    else if (g4L4.innerHTML == "") {g4L4.innerHTML = "H";}
+    else if (g4L5.innerHTML == "") {g4L5.innerHTML = "H";}
+    else if (g5L1.innerHTML == "") {g5L1.innerHTML = "H";}
+    else if (g5L2.innerHTML == "") {g5L2.innerHTML = "H";}
+    else if (g5L3.innerHTML == "") {g5L3.innerHTML = "H";}
+    else if (g5L4.innerHTML == "") {g5L4.innerHTML = "H";}
+    else if (g5L5.innerHTML == "") {g5L5.innerHTML = "H";}
+    else if (g6L1.innerHTML == "") {g6L1.innerHTML = "H";}
+    else if (g6L2.innerHTML == "") {g6L2.innerHTML = "H";}
+    else if (g6L3.innerHTML == "") {g6L3.innerHTML = "H";}
+    else if (g6L4.innerHTML == "") {g6L4.innerHTML = "H";}
+    else {g6L5.innerHTML = "H";}
+}
+
+let hKeyboardButton = document.querySelectorAll("button.keyboard-letter")[15];
+hKeyboardButton.addEventListener("click", guessH);
+
 
 /*
 let eraseButton = document.getElementsByClassName("keyboard-button")[1];
