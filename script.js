c965657c60a3604db90dd44f3f59c34c512d5249
y <johndoe@example.com>
2022-06-02 14:53:59 +0100

    Added functionality to S

--- script.js
+++ script.js
@@ -6236,7 +6236,41 @@ aKeyboardButton.addEventListener("click", guessA);
 
 
 
+function guessS () {
+    if (g1L1.innerHTML == "") {g1L1.innerHTML = "S";}
+    else if (g1L2.innerHTML == "") {g1L2.innerHTML = "S";}
+    else if (g1L3.innerHTML == "") {g1L3.innerHTML = "S";}
+    else if (g1L4.innerHTML == "") {g1L4.innerHTML = "S";}
+    else if (g1L5.innerHTML == "") {g1L5.innerHTML = "S";}
+    else if (g2L1.innerHTML == "") {g2L1.innerHTML = "S";}
+    else if (g2L2.innerHTML == "") {g2L2.innerHTML = "S";}
+    else if (g2L3.innerHTML == "") {g2L3.innerHTML = "S";}
+    else if (g2L4.innerHTML == "") {g2L4.innerHTML = "S";}
+    else if (g2L5.innerHTML == "") {g2L5.innerHTML = "S";}
+    else if (g3L1.innerHTML == "") {g3L1.innerHTML = "S";}
+    else if (g3L2.innerHTML == "") {g3L2.innerHTML = "S";}
+    else if (g3L3.innerHTML == "") {g3L3.innerHTML = "S";}
+    else if (g3L4.innerHTML == "") {g3L4.innerHTML = "S";}
+    else if (g3L5.innerHTML == "") {g3L5.innerHTML = "S";}
+    else if (g4L1.innerHTML == "") {g4L1.innerHTML = "S";}
+    else if (g4L2.innerHTML == "") {g4L2.innerHTML = "S";}
+    else if (g4L3.innerHTML == "") {g4L3.innerHTML = "S";}
+    else if (g4L4.innerHTML == "") {g4L4.innerHTML = "S";}
+    else if (g4L5.innerHTML == "") {g4L5.innerHTML = "S";}
+    else if (g5L1.innerHTML == "") {g5L1.innerHTML = "S";}
+    else if (g5L2.innerHTML == "") {g5L2.innerHTML = "S";}
+    else if (g5L3.innerHTML == "") {g5L3.innerHTML = "S";}
+    else if (g5L4.innerHTML == "") {g5L4.innerHTML = "S";}
+    else if (g5L5.innerHTML == "") {g5L5.innerHTML = "S";}
+    else if (g6L1.innerHTML == "") {g6L1.innerHTML = "S";}
+    else if (g6L2.innerHTML == "") {g6L2.innerHTML = "S";}
+    else if (g6L3.innerHTML == "") {g6L3.innerHTML = "S";}
+    else if (g6L4.innerHTML == "") {g6L4.innerHTML = "S";}
+    else {g6L5.innerHTML = "S";}
+}
 
+let sKeyboardButton = document.querySelectorAll("button.keyboard-letter")[11];
+sKeyboardButton.addEventListener("click", guessS);
 
 
 /*
